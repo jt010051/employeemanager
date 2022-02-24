6e3943105b5cad723d85cb57f3f55e2c6094277c
jt010051 <jont26.smith@hcl.com>
2022-02-24 15:06:02 -0500

    Repo, Service, Controller, and Database

---
+++ src/main/java/com/getarrays/employeemanager/exceptions/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.getarrays.employeemanager.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+public  UserNotFoundException(String message) {
+	
+	super(message);
+}
+}
---
+++ src/main/java/com/getarrays/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,15 @@
+package com.getarrays.employeemanager.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.getarrays.employeemanager.domain.Employee;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long>{
+
+	void deleteEmployeeById(Long id);
+
+	Optional<Employee> findEmployeeById(Long id);
+
+}
