af3ba46e6801edfed44bfe7101329a851032d87c
jt010051 <jont26.smith@hcl.com>
2022-03-02 16:01:26 -0500

    Fixed delete mapping with @Transactional tag

--- src/main/java/com/getarrays/employeemanager/EmployeeResource.java
+++ src/main/java/com/getarrays/employeemanager/EmployeeResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -59,6 +60,7 @@ public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
 
 }
 @DeleteMapping("/delete/{id}")
+@Transactional
 public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id){
 	
 	employeeService.deleteEmployee(id);
